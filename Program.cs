a98ba600db3387d4ad984e0ea675fa4e947a1525
ahmedms2104 <ahmedms2104@gmail.com>
2024-12-17 17:55:13 +0300

    exam

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+class Ahmed {
+// See https://aka.ms/new-console-template for more information
+static void Main(){
+    //1
+string name = "Ahmed almohammadi" ;
+Console.WriteLine(name);}
+
+//3
+int []G=[5,4,3,2,7];
+
+//2
+ static int l( int num1=4,int  num2=6){
+  num1 =4;
+  num2 = 6;
+     Console.WriteLine(6 + 4);
+}
+
+}
+
+class Program{
+
+    static void Main(){
+       //5
+        string dgree = Console.ReadLine();
+        switch(dgree){
+            case "A" :
+            Console.WriteLine("Excellent");
+            break;
+            case "B" :
+            Console.WriteLine("Very Good");
+            break;
+            case "C" :
+            Console.WriteLine("Good");
+            break;
+            case "D":
+            Console.WriteLine("need Improvement");
+            break;
+            case "F":
+            Console.WriteLine("Fail");
+            break;
+        } 
+        //4;
+        int g=int.maxValue(1,5);
+        
+    }
+}
